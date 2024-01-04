22b198f017c30b5432f4d0f0daeee7828dc238e4
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-01-04 14:26:59 +0530

    update home

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,6 @@ import { useState, useEffect } from "react";
 import BlogList from "./Blog-list";
 
 export default function Home(){
-const [blogs, setBlogs] = useState([
-    {title:"My new title 1", body:"sample body of the blog..", author:"siyam", id:1},
-    {title:"My new title 2", body:"sample body of the blog..", author:"Fayas", id:2},
-    {title:"My new title 3", body:"sample body of the blog..", author:"Jamseeth", id:3}
-]);
-
-const [name, setName] = useState("siyam");
-
-useEffect(() => {
-    console.log("use effect runs..");
-    console.log(name);
-
-},[name]);
 
 const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -23,10 +10,11 @@ const handleDelete = (id) => {
 
     return(
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs..." handleDelete={handleDelete}/>
+            {isLoading && <p>Loading...</p>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs..." handleDelete={handleDelete}/>}
             {/* <BlogList blogs={blogs.filter((blog)=> blog.author === "siyam")} title={"Siyam's Blog"}/> */}
-            <p>{name}</p>
-            <button onClick={() => setName("fayas")}>change name</button>
+            {/* <p>{name}</p> */}
+            {/* <button onClick={() => setName("fayas")}>change name</button> */}
         </div>
     )
 }
